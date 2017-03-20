2681bd79e5512e525dedb7d5f71fe2f80df9e46a
lihlepaie <pasielihle@gmail.com>
2017-03-20 16:17:49 +0200

    making changes on my js file

--- registration_number.js
+++ registration_number.js
@@ -4,19 +4,20 @@ var regNumbers = document.querySelector('.regNumbers');
 var province = document.querySelector('#province');
 
 button.onclick = function() {
-
+if (input.value.length !== 0) {
     var text = input.value;
     var li = document.createElement('li');
     li.textContent = text;
 
     regNumbers.appendChild(li)
 }
+}
 province.addEventListener('change', function(e) {
     var allRegnum = document.querySelectorAll('li');
 
     for (var i = 0; i < allRegnum.length; i++) {
         if (e.target.value === 'ALL') {
-          
+
             allRegnum[i].style.display = 'block'
         } else if (allRegnum[i].innerHTML.startsWith(e.target.value)) {
 

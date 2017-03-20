bbdd1cd8912d99ef7f809705200f3fdd77733cde
lihlepaie <pasielihle@gmail.com>
2017-03-20 15:16:00 +0200

    new file

--- registration_number.js
+++ registration_number.js
@@ -1,6 +1,7 @@
 var button = document.getElementById('add');
 var input = document.querySelector('#regNum');
 var regNumbers = document.querySelector('.regNumbers');
+var province = document.querySelector('#province');
 
 button.onclick = function() {
 
@@ -10,3 +11,16 @@ button.onclick = function() {
 
     regNumbers.appendChild(li)
 }
+province.addEventListener('change', function(e) {
+    var allRegnum = document.querySelectorAll('li');
+
+    for (var i = 0; i < allRegnum.length; i++) {
+        if (e.target.value === 'ALL') {
+            allRegnum[i].style.display = 'block'
+        } else if (allRegnum[i].innerHTML.startsWith(e.target.value)) {
+            allRegnum[i].style.display = 'block';
+        } else {
+            allRegnum[i].style.display = 'none';
+        }
+    }
+})
